b75f30b33fbd1516dcbdcd7a0b77df7ace80f8fd
Valentyn <vrubliuk@gmail.com>
2018-08-04 17:49:19 +0300

    Add the authentication page

--- src/containers/Authentication/Authentication.jsx
+++ src/containers/Authentication/Authentication.jsx
@@ -1,9 +1,50 @@
-import React, {Component} from "react";
-import './Authentication.css'
+import React, { Component } from "react";
+import "./Authentication.css";
 
 class Authentication extends Component {
+  state = {
+    email: "",
+    password: "",
+    error: false
+  };
+
+  handleInput = (e, type) => {
+    this.setState({
+      [type]: e.target.value
+    });
+  };
+
+  showError = () => {
+    this.setState({
+      error: true
+    });
+    setTimeout(() => {
+      this.setState({
+        error: false
+      });
+    }, 2000);
+  };
+
   render() {
-    return <div>Authentication</div>;
+    const errorMessage = this.state.error ? <div className="Authentication__Error">Incorrect email address or password</div> : null;
+    return (
+      <div className="Authentication__Wrapper">
+        <div className="Authentication">
+          <div className="Authentication__Header">Log in to Calendar</div>
+          <form className="Authentication__Main">
+            <div className="Authentication__Label">Email address</div>
+            <input className="Authentication__Input" type="email" required value={this.state.email} onChange={e => this.handleInput(e, "email")} />
+            <div className="Authentication__Label">Password</div>
+            <input className="Authentication__Input" type="password" required value={this.state.password} onChange={e => this.handleInput(e, "password")} />
+            <br />
+            <button className="Authentication__Button" type="submit" onClick={this.showError}>
+              Log in
+            </button>
+          </form>
+          {errorMessage}
+        </div>
+      </div>
+    );
   }
 }
 
