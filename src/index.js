b781c22c0b084fe4fd41306bac90db81b83e5247
Valentyn <vrubliuk@gmail.com>
2018-08-05 12:16:15 +0300

    Add middleware

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import App from "./App.jsx";
 import registerServiceWorker from "./registerServiceWorker";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { createStore, combineReducers } from "redux";
-// import reducer from "./store/reducer";
-
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import history from "./store/reducers/history";
 import employees from "./store/reducers/employees";
 import temporary from "./store/reducers/temporary";
@@ -24,7 +22,20 @@ const rootReducer = combineReducers({
   days
 });
 
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const logger = store => {
+  return next => {
+    return action => {
+      console.log('[Middleware Dispatching]', action);
+      const result = next(action);
+      console.log('[Middleware next state]', store.getState());
+      return result;
+    }
+  }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)));
 
 ReactDOM.render(
   <Provider store={store}>
