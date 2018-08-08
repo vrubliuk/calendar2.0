083179623b86591e771dc56569bcfc461b6ce3f8
Valentyn <vrubliuk@gmail.com>
2018-08-08 17:33:43 +0300

    Rename files

--- src/store/actions/API.js
+++ src/store/actions/API.js
@@ -1,26 +1,10 @@
-import axios from "axios";
+import * as API from "../utility/endpoints";
 import { setEmployees } from "./employees";
 import { setLastUpdate } from "./history";
 
-const getEmployees = () => {
-  return axios.get("https://calendar-0002.firebaseio.com/employees.json");
-};
-
-const putEmployees = employees => {
-  return axios.put("https://calendar-0002.firebaseio.com/employees.json", employees);
-};
-
-const getLastUpdate = () => {
-  return axios.get("https://calendar-0002.firebaseio.com/lastUpdate.json");
-};
-
-const putLastUpdate = date => {
-  return axios.put("https://calendar-0002.firebaseio.com/lastUpdate.json", date);
-};
-
 export const fetchDatabase = () => {
   return dispatch => {
-    Promise.all([getEmployees(), getLastUpdate()]).then(res => {
+    Promise.all([API.getEmployees(), API.getLastUpdate()]).then(res => {
       dispatch(setEmployees(res[0].data));
       dispatch(setLastUpdate(res[1].data));
     });
--- src/store/actions/employees.js
+++ src/store/actions/employees.js
@@ -1,5 +1,5 @@
 import * as actionTypes from "./actionTypes"
-import * as API from "../utility/API"
+import * as API from "../utility/endpoints"
 
 export const setEmployees = (employees) => {
   return {
