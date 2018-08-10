63cbf0e436234d40b707e8b2e0f5a676a44e7909
Valentyn <vrubliuk@gmail.com>
2018-08-10 19:57:15 +0300

    Add action creators for the saving indicator

--- src/store/actions/status.js
+++ src/store/actions/status.js
@@ -1,9 +1,20 @@
-import * as actionTypes from "./actionTypes"
+import * as actionTypes from "./actionTypes";
 
-export const setLastUpdate = (date) => {
+export const setLastUpdate = date => {
   return {
     type: actionTypes.SET_LAST_UPDATE,
     date
-  }
-}
+  };
+};
 
+export const showSavingIndicator = () => {
+  return {
+    type: actionTypes.SHOW_SAVING_INDICATOR
+  };
+};
+
+export const hideSavingIndicator = () => {
+  return {
+    type: actionTypes.HIDE_SAVING_INDICATOR
+  };
+};
